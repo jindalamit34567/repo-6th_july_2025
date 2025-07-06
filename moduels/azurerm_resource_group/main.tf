80cb934b1f4d27a12193c8d514ca60e57ee3f746
Amit Jindal <AmitJindal@princenagaroutlook.onmicrosoft.com>
2025-07-06 14:28:38 +0530

    new rg 101 aded

--- moduels/azurerm_resource_group/main.tf
+++ moduels/azurerm_resource_group/main.tf
@@ -2,3 +2,9 @@ resource "azurerm_resource_group" "resource_group" {
   name     = var.resource_group_name
   location = var.resource_group_location
 }
+
+# New rg added for testing purposes
+resource "azurerm_resource_group" "resource_feature101" {
+  name     = var.resource_group_name
+  location = var.resource_group_location
+}
