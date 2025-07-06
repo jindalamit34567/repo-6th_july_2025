3109aa3e44b05810dd2ac290e09a0960288d317d
Amit Jindal <AmitJindal@princenagaroutlook.onmicrosoft.com>
2025-07-06 14:34:22 +0530

    new rg 102 aded

--- moduels/azurerm_resource_group/main.tf
+++ moduels/azurerm_resource_group/main.tf
@@ -2,3 +2,9 @@ resource "azurerm_resource_group" "resource_group" {
   name     = var.resource_group_name
   location = var.resource_group_location
 }
+
+# New rg added for testing purposes-rg102
+resource "azurerm_resource_group" "resource_group-102-new-rg" {
+  name     = var.resource_group_name
+  location = var.resource_group_location
+}
